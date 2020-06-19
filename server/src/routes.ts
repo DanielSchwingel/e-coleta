4bb7e383c49a94f747a30aa1f1bb18495a5ce7bc
unknown <danielfilipeschwingel@gmail.com>
2020-06-19 17:47:40 -0300

    Created more routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import knex from './database/connection';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) =>{
-    return response.json({
-        message: 'Teste'
-    })
-});
+// index, show, create, update, delete
+
+routes.get('/items', itemsController.index);
+
+routes.get('/points/:id', pointsController.show);
+routes.post('/points', pointsController.create);
 
 export default routes;
