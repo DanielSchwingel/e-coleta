d215aa8556d26468da445e8590059a431d752dfe
unknown <danielfilipeschwingel@gmail.com>
2020-06-19 23:58:28 -0300

    Finalized backend

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -4,6 +4,24 @@ import knex from '../database/connection';
 
 class PointsController {
 
+    async index(request : Request, response : Response) {
+        const { city, uf, items } = request.query;
+
+        const serializedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
+
+        const points = await knex('points')
+            .join('point_items', 'point_items.point_id', '=', 'points.id')
+            .whereIn('point_items.item_id', serializedItems)
+            .where('points.city', String(city))
+            .where('points.uf', String(uf))
+            .distinct()
+            .select('points.*')
+        
+        return response.json(points);
+    }
+
     async show(request : Request, response : Response) {
         const { id } = request.params; 
 
@@ -17,11 +35,10 @@ class PointsController {
             .where('point_items.point_id', id)
             .select('items.title');
 
-
         return response.json({point, items});
     }
 
-    async create(request: Request, response : Response) {
+    async create(request : Request, response : Response) {
         const { 
             name, 
             email,
@@ -36,7 +53,7 @@ class PointsController {
         const trx = await knex.transaction(); 
         
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name, 
             email, 
             whatsapp, 
@@ -58,6 +75,8 @@ class PointsController {
         })
     
         await trx('point_items').insert(pointItems);
+
+        await trx.commit();
     
         return response.json({id: point_id, ...point});
     }
