4fd2f9422f01bc9d34f535c14901906b08eec36b
unknown <danielfilipeschwingel@gmail.com>
2020-06-24 16:45:13 -0300

    Project Finalized

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes';
 import path from 'path';
+import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes); 
+app.use(routes);
+
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(3333, ()=>{
-    console.log('Server is running on port 3333');
-})
+app.use(errors());
+
+app.listen(3333);
